70fc7733da840a60b0b6cba0314c8e92034b27a8
jesamonen <jesamonen01@gmail.com>
2026-07-16 01:40:59 +0100

    commit

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -15,16 +15,11 @@ menuButton.addEventListener("click", () => {
 // ===============================
 // Footer Information
 // ===============================
-const year = new Date().getFullYear();
+document.getElementById("current-year").textContent =
+    new Date().getFullYear();
 
-const footer = document.querySelector("footer");
-
-footer.innerHTML += `
-    <p>&copy; ${year} Lagos Chamber of Commerce</p>
-    <p>Jesam Onen | WDD 231</p>
-    <p>Last Modified: ${document.lastModified}</p>
-`;
-
+document.getElementById("lastModified").textContent =
+    document.lastModified;
 
 // ===============================
 // Highlight Current Page
