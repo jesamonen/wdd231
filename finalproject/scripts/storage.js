a702428aafb9a4b9f7e91562536df06acd2ed679
jesamonen <jesamonen01@gmail.com>
2026-08-06 14:14:04 +0100

    commit

--- finalproject/scripts/storage.js
+++ finalproject/scripts/storage.js
@@ -1,67 +1,34 @@
-// ==========================================
 // storage.js
-// Handles Local Storage
-// ==========================================
 
 const STORAGE_KEY = "favoriteServices";
 
-/**
- * Get all favorite services
- * @returns {Array}
- */
+// Get all favorites
 export function getFavorites() {
     return JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
 }
 
-/**
- * Save favorites to Local Storage
- * @param {Array} favorites
- */
+// Save favorites
 export function saveFavorites(favorites) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(favorites));
 }
 
-/**
- * Add a service to favorites
- * @param {number} id
- */
+// Add a favorite
 export function addFavorite(id) {
-
     const favorites = getFavorites();
 
-    if (!favorites.includes(id)) {
-
-        favorites.push(id);
-
-        saveFavorites(favorites);
-
-        return true;
-    }
-
-    return false;
-}
-
-/**
- * Remove a favorite
- * @param {number} id
- */
-export function removeFavorite(id) {
-
-    let favorites = getFavorites();
-
-    favorites = favorites.filter(item => item !== id);
+    favorites.push(id);
 
     saveFavorites(favorites);
-
 }
 
-/**
- * Check if a service is already a favorite
- * @param {number} id
- * @returns {boolean}
- */
+// Remove a favorite
+export function removeFavorite(id) {
+    const favorites = getFavorites().filter(item => item !== id);
+
+    saveFavorites(favorites);
+}
+
+// Check if a service is a favorite
 export function isFavorite(id) {
-
     return getFavorites().includes(id);
-
 }
